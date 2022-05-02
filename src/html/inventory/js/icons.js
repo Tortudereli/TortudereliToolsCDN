71b75127e5e19e0b2285fac64451e56f439fe9eb
Tortudereli <tortudereli@outlook.com.tr>
2022-05-02 22:28:49 +0300

    Delete :(

--- src/html/inventory/js/icons.js
+++
@@ -1,6 +0,0 @@
-async function getData() {
-    var iconData = ipcRenderer.sendSync("get", `/lol-inventory/v2/inventory/SUMMONER_ICON`)['body'];
-    iconData.forEach(element => {
-        document.getElementById('inventoryArea').innerHTML += `<img src="http://ddragon.leagueoflegends.com/cdn/${versionData}/img/profileicon/${element['itemId']}.png" alt="">`;
-    });
-}
